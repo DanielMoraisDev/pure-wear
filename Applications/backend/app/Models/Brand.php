ecf488204be4f4a37934e4a4f685d7ca4a326c43
DanielMoraisDev <danielmorais.dev@gmail.com>
2026-04-13 17:52:04 -0300

    feat: added models of category and brand

--- Applications/backend/app/Models/Brand.php
+++ Applications/backend/app/Models/Brand.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Brand extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * Os atributos que podem ser preenchidos em massa.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'status',
+    ];
 }
