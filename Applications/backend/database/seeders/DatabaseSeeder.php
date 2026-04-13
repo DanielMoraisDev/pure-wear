46bb517dbd90b91c2c0961564dd164d0278cb4ee
DanielMoraisDev <danielmorais.dev@gmail.com>
2026-04-13 14:13:48 -0300

    feat: added seeders

--- Applications/backend/database/seeders/DatabaseSeeder.php
+++ Applications/backend/database/seeders/DatabaseSeeder.php
@@ -3,23 +3,23 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Cria o Admin
         User::factory()->create([
             'name' => 'Admin',
+            'role' => 'admin',
             'email' => 'admin@example.com',
+        ]);
+
+        // Chama os novos seeders
+        $this->call([
+            CategorySeeder::class,
+            BrandSeeder::class,
         ]);
     }
 }
