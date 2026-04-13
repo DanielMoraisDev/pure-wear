12223a4f980afba74b714dd7ca431019f98ced12
DanielMoraisDev <danielmorais.dev@gmail.com>
2026-04-13 18:11:23 -0300

    feat: added some models, seeders and migrations to product use

---
+++ Applications/backend/app/Models/Product.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'price',
+        'compare_price',
+        'description',
+        'short_description',
+        'image',
+        'category_id',
+        'brand_id',
+        'qty',
+        'sku',
+        'barcode',
+        'status',
+        'is_featured',
+    ];
+
+    // Relacionamentos
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+}
